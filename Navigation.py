1919c8bbc3aec6c4595da2f93a7bd67c7f0f5191
Keng <siwat.keng.pongpanit@gmail.com>
2021-01-23 23:17:35 +0700

    add calculate all path

--- Navigation.py
+++ Navigation.py
@@ -12,6 +12,7 @@ class Navigation:
         self.current_position = None
         self.goal = None
         self.path = deque()
+        self.all_path = tuple()
 
     def set_position(self, x: int, y: int):
         if (x, y) not in self.graph:
@@ -31,24 +32,27 @@ class Navigation:
     def del_path(self, u: tuple, v: tuple):
         self.graph[u] = tuple(e for e in self.graph[u] if e != v)
 
-    def del_all_path(self, node: tuple):
+    def del_node(self, node: tuple):
         self.graph.pop(node, None)
-        for (x, y) in self.graph:
-            self.graph[(x, y)] = tuple(
-                e for e in self.graph[(x, y)] if e != node)
 
-    def calculate_path(self):
+    def remove_all_path(self, node: tuple):
+        self.graph.pop(node, None)
+        for point in self.graph:
+            self.graph[point] = tuple(
+                e for e in self.graph[point] if e != node)
+
+    def calculate_shortest_path(self):
         if not self.current_position or not self.goal:
             return False
         backtracker = {}
         queue = deque()
         queue.append(self.current_position)
         while len(queue):
-            (x, y) = queue.popleft()
-            for p in self.graph[(x, y)]:
+            point = queue.popleft()
+            for p in self.graph[point]:
                 if p not in backtracker:
                     queue.append(p)
-                    backtracker[p] = (x, y)
+                    backtracker[p] = point
                     if p == self.goal:
                         backtrack = p
                         self.path = deque()
@@ -57,6 +61,24 @@ class Navigation:
                             backtrack = backtracker[backtrack]
                         return True
         self.path = deque()
+        return False
+
+    def calculate_all_path(self):
+        if not self.current_position or not self.goal:
+            return False
+        queue = deque()
+        queue.append((self.current_position, {self.current_position}, tuple()))
+        found = tuple()
+        while len(queue):
+            point, visited, path = queue.popleft()
+            if point == self.goal:
+                found += (path, )
+            for p in self.graph[point]:
+                if p not in visited:
+                    queue.append((p, visited | {p}, path+(p,)))
+        if found:
+            self.all_path = found
+            return True
         return False
 
     def navigate(self):
