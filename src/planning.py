4f577154881d636cd211c0073606d65ce8da188f
Keng <siwat.keng.pongpanit@gmail.com>
2021-04-05 15:14:24 +0700

    add template

--- src/planning.py
+++ src/planning.py
@@ -1,22 +1,36 @@
 #!/usr/bin/env python3
 
 import json
-import queue
 import rospy
-import threading
-from datetime import datetime
 
+from modules.Navigation import *
 from robot_state.srv import *
+
+SET_GOAL = 0
+MOVE = 1
+SET_MAP = 2
+
+ROBOT_DIMENSION = 1
+
+navigation = Planning()
+diffdrive = DifferentialDrive(ROBOT_DIMENSION)
+memory = []
 
 def robot_state_callback(req):
 
-    print(type(req.req))
-    return str(datetime.now().timestamp())
+    commands = json.loads(req.req)
+    response = {}
+    if commands['type'] == SET_GOAL:
+        pass
+    elif commands['type'] == MOVE:
+        pass
+    elif commands['type'] == SET_MAP:
+        pass
+    return json.dumps(response)
 
 def main():
 
     rospy.init_node('planning_runner', anonymous=True)
-    
     rospy.Service('planning', Planning, robot_state_callback)
     
     rospy.spin()
@@ -25,5 +39,3 @@ def main():
 if __name__ == "__main__":
     print('started planning')
     main()
-
-    
